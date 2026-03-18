eabe00d7c094246ce6784ca5bbc7c43702b3d225
harshajagtap2021-cpu <harshajagtap2021@gmail.com>
2026-03-18 15:15:00 +0530

    Add files via upload

---
+++ Assignment2/Que6.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+void main()
+{
+    char ch='1';
+
+    if(ch >= 'A' && ch <= 'Z')
+        printf("Charcter is Uppercase letter");
+    else 
+        printf("Charcter is Lowercase letter");
+}
