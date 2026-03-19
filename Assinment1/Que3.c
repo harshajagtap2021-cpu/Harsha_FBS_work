5acd237523f3fafe34307dce2b31a61ec71bc8b1
harshajagtap2021-cpu <harshajagtap2021@gmail.com>
2026-03-19 13:42:53 +0530

    Delete Assinment1 directory

--- Assinment1/Que3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int C=25;
-int F;
-F = (C *9/5) + 32;
-	printf("result %d",F);
-}
--- Assinment1/Que8.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int l=5,w=10;
-int peri;
-peri=2*(l+w);
-	printf("Perimeter of Rectangle = %d",peri);
-
-	}
