f9087f5709207542f6706734f1cbb3e47c6ad22c
HARSHA JAGTAP <harshajagtap2021@gmail.com>
2026-03-30 16:08:46 +0530

    Add files via upload

---
+++ Assignment5/Que10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+
+            // Border + diagonals
+            if(i == 1 || i == n || j == 1 || j == n || i == j ) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+
+        }
+        printf("\n");
+    }
+
+  
+}
